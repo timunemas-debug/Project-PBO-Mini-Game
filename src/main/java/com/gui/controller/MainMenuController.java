3cbc248fd4f0979666348ab6a0765b988047b9f3
Jeremy <jeremyputradarma1@gmail.com>
2026-05-20 16:37:23 +0700

    membuat background di css

--- src/main/java/com/gui/controller/MainMenuController.java
+++ src/main/java/com/gui/controller/MainMenuController.java
@@ -6,18 +6,57 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainMenuController {
 
     @FXML
+    private StackPane root;
+    @FXML
+    private Button btnStart;
+    @FXML
+    private Button btnExit;
+    @FXML
+    private ImageView bgImage;
+
+    @FXML
+    public void initialize(){
+        System.out.println("INITIALIZE DIPANGGIL");
+        var stream = getClass().getResourceAsStream("/Images/BackgroundDisplay.jpg");
+
+        if(stream == null){
+            System.out.println("Gambar tidak ditemukan");
+            return;
+        }
+
+        Image bg = new Image(stream);
+        bgImage.setImage(bg);
+
+        bgImage.fitWidthProperty().bind(root.widthProperty());
+        bgImage.fitHeightProperty().bind(root.heightProperty());
+
+        System.out.println("BACKGROUND BERHASIL DI SET");
+
+        btnStart.getStyleClass().clear();
+        btnStart.getStyleClass().add("btn-start");
+            
+        btnExit.getStyleClass().clear();
+        btnExit.getStyleClass().add("btn-exit");
+
+        }
+
+    @FXML
     private void startGame(ActionEvent event) throws Exception {
 
-        Parent root = FXMLLoader.load(
+        Parent newRoot = FXMLLoader.load(
                 getClass().getResource("/fxml/game.fxml")
         );
 
-        Scene scene = new Scene(root, 1280,720);
+        Scene scene = new Scene(newRoot, 1280,720);
         Stage stage = (Stage)((Node)event.getSource())
                 .getScene()
                 .getWindow();
